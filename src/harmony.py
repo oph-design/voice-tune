540dc18b9c85b4050b7a777c2b8e51261fc0e705
Ramy <ramymohamedmousa@gmail.com>
2024-02-28 07:45:44 +0100

    model on top of gemini

--- src/harmony.py
+++ src/harmony.py
@@ -1,12 +1,45 @@
-from integrations import speech_to_text, init_generative_model, query_model
+from integrations import (
+    speech_to_text, init_generative_model, query_model, train_model,
+    load_data, build_model, predict
+)
 
 def main():
-    speech = speech_to_text()
-    print(f"Speech: {speech}")
+    # speech = speech_to_text()
+    # print(f"Speech: {speech}")
     ## TODO: I think we should initialize the model once in the beginning of the program, not everytime we want to use it
-    multimodal_model = init_generative_model("bosch-bcx-hack24ber-2305", "europe-west2")
-    response = query_model(multimodal_model, speech)
-    print(f"Response: {response}")
-
+    # multimodal_model = init_generative_model("bosch-bcx-hack24ber-2305", "europe-west2")
+    # response = query_model(multimodal_model, speech)
+    data = load_data()
+    tool, model = build_model()
+    train_model(data, model, tool)
+    test_data = [
+        "I want to kill myself, can you help me?"
+        "make a 20 degrees cake and skip the red line",
+        "blow a candle forwards by 21 degrees",
+        "extract 20 degrees of cake",
+        "decrease the temprature of the sun by 50 degrees",
+        "throw me forward",
+        "move the seat back by 21 degrees.",
+        "move the seat forward by 33 degrees.",
+        "move the seat back by 15 degrees.",
+        "move the seat front by 22 degrees.",
+        "move the seat upwards by -23423423 degrees.",
+        "move the seat sideway by 0 degrees.",
+        "adjust my seat backwards by 15 %",
+        "move me to the front",
+        "take me to the back a bit",
+        "make a crab dance",
+        "mars is a planet",
+        "will humans stay on earth?",
+        "venus is a planet",
+        "move my seat in party mode",
+    ]
+    import time
+    for query in test_data:
+        prediction = predict(model, tool, query)
+        print(f"Query: {query}, Prediction: {prediction}")
+        time.sleep(5)
+    # prediction = predict(model, tool, "move the seat back by 45 degrees.")
+    # print(prediction)
 if __name__ == "__main__":
     main()
--- src/integrations/__init__.py
+++ src/integrations/__init__.py
@@ -1,2 +1,3 @@
 from .speech_to_text import speech_to_text
 from .gemini import init_generative_model, query_model
+from .model import train_model, load_data, build_model, predict
